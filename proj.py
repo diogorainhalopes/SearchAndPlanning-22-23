050372f6d1f9c5b22406a9e87e54ae58f05ff68e
diogorainhalopes <diogo.rainha.lopes@tecnico.ulisboa.pt>
2022-10-04 19:38:56 +0100

    changed minimize constraint

--- proj.py
+++ proj.py
@@ -17,7 +17,7 @@ def main():
         sys.exit("Usage: proj initial_graph.txt initial_scenario.txt")
     graph = parse_graph(sys.argv[1])
     scenario = parse_scenario(sys.argv[2])
-    os.system(f"minizinc {MODEL} {graph} {scenario} > solution.txt")
+    os.system(f"minizinc --solver Chuffed {MODEL} {graph} {scenario} > solution.txt")
     os.system(f"type solution.txt")
 
 if __name__ == "__main__":
